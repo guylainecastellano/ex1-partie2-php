bda35e8275b051bc8f25ad464cef885284009cd7
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-06 14:00:20 +0200

    ex1 partie2 php

--- index.php
+++ index.php
@@ -6,19 +6,19 @@ $a = 45;
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8"/>
-  <title>ex1 partie1 php</title>
+  <title>ex1 partie2 php</title>
 </head>
 <body>
   <p>
     <?php
     if ($a > $age) {
-  echo "Vous êtes majeur";
-}  if ($a == $age) {
-  echo "Vous êtes majeur";
-} if ($a < $age) {
-  echo "Vous êtes mineur";
+      echo "Vous êtes majeur";
+    }  if ($a == $age) {
+      echo "Vous êtes majeur";
+    } if ($a < $age) {
+      echo "Vous êtes mineur";
 
-  }
+    }
     ?>
   </p>
 </body>
